90fa43624e9c971804a87942016dd4246f39419c
Wim Leers <work@wimleers.com>
2008-02-09 17:32:24 +0000

    More usability improvements, now for the settings that depend on the multiple select setting.

--- modules/taxonomy.js
+++ modules/taxonomy.js
@@ -5,21 +5,46 @@ Drupal.HierarchicalSelectTaxonomyForm = {};
 Drupal.HierarchicalSelectTaxonomyForm.levelLabels = function() {
   var $statusSelect = $('select#edit-hierarchical-select-level-labels-level-labels-status');
 
-  var showHideLevelLabels = function() {    
+  var showHide = function(speed) {
+    if (speed === undefined) {
+      speed = 200;
+    }
     if ($statusSelect.val() ==  0) {
-      $('.hierarchical-select-level-label').parent().hide(0);
+      $('.hierarchical-select-level-label').parent().hide(speed);
     }
     else {
-      $('.hierarchical-select-level-label').parent().show(0);
+      $('.hierarchical-select-level-label').parent().show(speed);
     }
   };
 
-  $statusSelect.change(showHideLevelLabels);
-  showHideLevelLabels();
+  $statusSelect.change(showHide);
+  showHide(0);
+};
+
+Drupal.HierarchicalSelectTaxonomyForm.dependentOnMultipleSetting = function() {
+  var $statusSelect = $('#edit-hierarchical-select-multiple');
+
+  var showHide = function(speed) {
+    if (speed === undefined) {
+      speed = 200;
+    }
+    $('#edit-hierarchical-select-all-option, #edit-hierarchical-select-dropbox-dropbox-title, #edit-hierarchical-select-dropbox-dropbox-limit').each(function() {
+      if ($statusSelect.attr('checked') === undefined) {
+        $(this).parent().hide(speed);  
+      }
+      else {
+        $(this).parent().show(speed);
+      }
+    });
+  };
+
+  $statusSelect.change(showHide);
+  showHide(0);
 };
 
 if (Drupal.jsEnabled) {
   $(document).ready(function() {
     Drupal.HierarchicalSelectTaxonomyForm.levelLabels();
+    Drupal.HierarchicalSelectTaxonomyForm.dependentOnMultipleSetting();
   });
 }
